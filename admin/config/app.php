08e0798a2f41b8dd2b54c4aa48c35843d2b65c1f
habib27akbar <akbarca94@gmail.com>
2023-12-26 18:23:26 +0700

    captcha

--- admin/config/app.php
+++ admin/config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Mews\Captcha\CaptchaServiceProvider::class,
         /*
          * Package Service Providers...
          */
