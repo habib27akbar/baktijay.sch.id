cccc161def8ef958efcea9ac793b71ae33c0e7b7
habib27akbar <akbarca94@gmail.com>
2023-12-29 22:28:26 +0700

    slider

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\SliderModel;
+
+class HomeController extends Controller
+{
+
+    public function index()
+    {
+        $data_slider = SliderModel::all();
+
+        return view('home', compact('data_slider'));
+    }
+}
